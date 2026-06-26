3a8da7937f7d29322581aa6f457107c856f5d48d
glazba <szabitomi207@gmail.com>
2026-06-26 13:56:18 +0200

    Restructure project and initialize React frontend with Vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>BB-XChange</h1>;
+}
+
+export default App;
---
+++ frontend/src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+import App from "./App";
+import "./styles/globals.css";
+
+ReactDom.createRoot(document.getElementById("roow")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
